b34afc4e7a585c6e322e8abe0a7d267b07ad7387
GuilleDc <GuilleDc@DESKTOP-2C1CG3Q>
2025-05-31 16:13:03 -0500

    Subiendo Hola mundo

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,8 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo cruel!");
+        System.out.println("Hola mundo cruel!");
     }
+    
 }
